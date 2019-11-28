b89a99fa4224086002c71342c0cc15134246c50d
Zacck Osiemo <coderv63@gmail.com>
2019-11-28 07:36:18 +0200

    Adjust structure to support super long names

--- src/elm/Page/Dashboard/Community.elm
+++ src/elm/Page/Dashboard/Community.elm
@@ -425,8 +425,8 @@ viewAutoCompleteItem shared profile =
             shared.endpoints.ipfs
     in
     div [ class "autocomplete-item" ]
-        [ Avatar.view ipfsUrl profile.avatar "profile-img-avatar-select"
-        , p [ class "whitespace-no-wrap overflow-hidden" ]
+        [ div [ class "flex-grow-3" ] [ Avatar.view ipfsUrl profile.avatar "profile-img-avatar-select" ]
+        , div [ class "flex-grow-7" ]
             [ text (Eos.nameToString profile.accountName)
             , text " "
             , case profile.userName of
