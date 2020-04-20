d9391e4a7481419df23b6c35819e7bd2a2a6088b
Julien Lucca <julienlucca@gmail.com>
2020-04-20 17:00:19 -0300

    Fix transfer layout issues (#221)
    * Fix transfer layout issues
    
    * Transfer layout adjustments
    
    * Add "my balance" button to viewtransfer
    
    * more layout adjustments
    
    * Add back arrows for the transfer screen
    
    * add -90 rotation into tailwind config

--- src/elm/Page.elm
+++ src/elm/Page.elm
@@ -251,11 +251,11 @@ viewHeader { shared } title route =
     div [ class "w-full h-16 flex px-4 items-center bg-indigo-500" ]
         [ div [ class "flex container mx-auto" ]
             [ a
-                [ class "flex items-center"
+                [ class "flex items-center absolute"
                 , Route.href route
                 ]
                 [ Icons.back ""
-                , p [ class "ml-2 text-white text-sm" ]
+                , p [ class "ml-2 text-white text-sm hidden md:visible md:flex" ]
                     [ text (I18Next.t shared.translations "back") ]
                 ]
             , p [ class "mx-auto text-white" ] [ text title ]
