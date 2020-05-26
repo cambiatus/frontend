10b0008ce2647b181616e7e57fad1369a7bb1a1f
Rodrigo Leite <rodrigo@leite.dev>
2020-05-26 09:15:09 -0300

    Hide actions on community page if not available

--- src/elm/Page/Community.elm
+++ src/elm/Page/Community.elm
@@ -211,20 +211,23 @@ view loggedIn model =
 
                       else
                         text ""
-                    , div [ class "bg-white py-6 sm:py-8 px-3 sm:px-6 rounded-lg mt-4" ]
-                        ([ Page.viewTitle (t "community.objectives.title_plural") ]
-                            ++ List.indexedMap (viewObjective loggedIn model community)
-                                community.objectives
-                            ++ [ if canEdit then
-                                    viewObjectiveNew loggedIn editStatus community.symbol
+                    , if community.actions then
+                        div [ class "bg-white py-6 sm:py-8 px-3 sm:px-6 rounded-lg mt-4" ]
+                            ([ Page.viewTitle (t "community.objectives.title_plural") ]
+                                ++ List.indexedMap (viewObjective loggedIn model community)
+                                    community.objectives
+                                ++ [ if canEdit then
+                                        viewObjectiveNew loggedIn editStatus community.symbol
 
-                                 else
-                                    text ""
-                               ]
-                        )
+                                     else
+                                        text ""
+                                   ]
+                            )
+                        -- , Transfer.getTransfers (Just community)
+                        --     |> viewSections loggedIn model
 
-                    -- , Transfer.getTransfers (Just community)
-                    --     |> viewSections loggedIn model
+                      else
+                        div [] []
                     ]
                 ]
 
