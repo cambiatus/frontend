04e31074d5219de441ffb124e7e3782e7b1dafd8
Andrey Miskov <amiskov@gmail.com>
2020-07-23 15:15:33 +0300

    Improve community selector modal

--- src/elm/Session/LoggedIn.elm
+++ src/elm/Session/LoggedIn.elm
@@ -476,7 +476,7 @@ communitySelectorModal model =
         viewCommunityItem : Profile.CommunityInfo -> Html Msg
         viewCommunityItem c =
             div
-                [ class "flex items-center py-4 border-b text-body hover:pointer"
+                [ class "flex items-center p-4 text-body cursor-pointer hover:text-black hover:bg-gray-100"
                 , onClick <| SelectCommunity c.id
                 ]
                 [ img [ src (logoUrl c.logo), class "h-16 w-16 mr-5" ] []
@@ -490,24 +490,19 @@ communitySelectorModal model =
                     text ""
 
                 else
-                    div [ class "modal container" ]
-                        [ div [ class "modal-bg", onClick CloseCommunitySelector ] []
-                        , div [ class "modal-content overflow-auto", style "height" "65%" ]
-                            [ div [ class "w-full" ]
-                                [ p [ class "w-full font-bold text-heading text-2xl" ]
-                                    [ text_ "menu.community_selector.title" ]
-                                , button
-                                    [ onClick CloseCommunitySelector ]
-                                    [ Icons.close "absolute fill-current text-gray-400 top-0 right-0 m-4"
-                                    ]
-                                , p [ class "text-body w-full font-sans -mt-2 mb-2" ]
-                                    [ text_ "menu.community_selector.body"
-                                    ]
-                                , div [ class "w-full overflow-scroll" ]
-                                    (List.map viewCommunityItem pro.communities)
+                    Modal.initWith
+                        { closeMsg = CloseCommunitySelector
+                        , isVisible = True
+                        }
+                        |> Modal.withHeader (t "menu.community_selector.title")
+                        |> Modal.withBody
+                            [ p []
+                                [ text_ "menu.community_selector.body"
                                 ]
+                            , div [ class "w-full overflow-y-auto divide-y divide-gray-300" ]
+                                (List.map viewCommunityItem pro.communities)
                             ]
-                        ]
+                        |> Modal.toHtml
 
             _ ->
                 text ""
