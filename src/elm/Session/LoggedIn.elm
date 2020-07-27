53cb4f0b0873d38aed44999dd9408172c4efa07e
Lucca <julienlucca@gmail.com>
2020-07-27 13:38:18 -0300

    Fix warning

--- src/elm/Session/LoggedIn.elm
+++ src/elm/Session/LoggedIn.elm
@@ -43,7 +43,7 @@ import Graphql.Http
 import Graphql.Operation exposing (RootSubscription)
 import Graphql.SelectionSet exposing (SelectionSet)
 import Html exposing (Html, a, button, div, footer, img, nav, p, span, text)
-import Html.Attributes exposing (class, classList, src, style, type_, value)
+import Html.Attributes exposing (class, classList, src, style, type_)
 import Html.Events exposing (onClick, onMouseEnter)
 import Http
 import I18Next exposing (Delims(..), Translations, t)
