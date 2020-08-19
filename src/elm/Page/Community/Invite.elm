8e14641033a1fa964448daf36a906882550387a6
Andrey Miskov <amiskov@gmail.com>
2020-08-19 21:10:02 +0300

    Improve invitation process for inviter and invitee (#339)
    * Append port to invitation URL if it's presented in the URL
    
    * Improve code redability
    
    * Check if invitee is already inviter's community member

--- src/elm/Page/Community/Invite.elm
+++ src/elm/Page/Community/Invite.elm
@@ -10,48 +10,31 @@ module Page.Community.Invite exposing
 import Api
 import Api.Graphql
 import Community exposing (Invite)
-import Eos.Account as Eos
+import Eos exposing (symbolToString)
+import Eos.Account exposing (nameToString)
 import Graphql.Http
-import Html exposing (Html, button, div, img, span, text)
+import Html exposing (Html, button, div, img, p, span, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 import Http
-import I18Next exposing (t)
 import Page exposing (Session(..), toShared)
 import Profile exposing (Profile)
 import Route
 import Session.LoggedIn as LoggedIn exposing (External)
-import Session.Shared exposing (Shared)
+import Session.Shared exposing (Translators)
 import UpdateResult as UR
 import View.Modal as Modal
 
 
-init : Session -> String -> ( Model, Cmd Msg )
-init session invitationId =
-    ( initModel invitationId
-    , Api.Graphql.query
-        (toShared session)
-        (Community.inviteQuery invitationId)
-        CompletedLoad
-    )
+
+-- TYPES
 
 
-initModel : String -> Model
-initModel invitationId =
-    { status = Loading
-    , confirmationModal = Closed
-    , invitationId = invitationId
-    }
+type alias InvitationId =
+    String
 
 
-type alias Model =
-    { status : Status
-    , confirmationModal : ModalStatus
-    , invitationId : String
-    }
-
-
-type Status
+type PageStatus
     = Loading
     | NotFound
     | Failed (Graphql.Http.Error (Maybe Invite))
@@ -64,23 +47,63 @@ type ModalStatus
     | Open
 
 
+
+-- MODEL
+
+
+type alias Model =
+    { pageStatus : PageStatus
+    , confirmationModalStatus : ModalStatus
+    , invitationId : InvitationId
+    }
+
+
+
+-- INIT
+
+
+initModel : String -> Model
+initModel invitationId =
+    { pageStatus = Loading
+    , confirmationModalStatus = Closed
+    , invitationId = invitationId
+    }
+
+
+init : Session -> String -> ( Model, Cmd Msg )
+init session invitationId =
+    ( initModel invitationId
+    , Api.Graphql.query
+        (toShared session)
+        (Community.inviteQuery invitationId)
+        CompletedLoad
+    )
+
+
+
+-- VIEW
+
+
 view : Session -> Model -> { title : String, content : Html Msg }
 view session model =
     let
         shared =
             toShared session
 
+        { t } =
+            shared.translators
+
         title =
-            case model.status of
+            case model.pageStatus of
                 Loaded invite ->
                     let
                         inviter =
                             invite.creator.userName
-                                |> Maybe.withDefault (Eos.nameToString invite.creator.account)
+                                |> Maybe.withDefault (nameToString invite.creator.account)
                     in
                     inviter
                         ++ " "
-                        ++ t shared.translations "community.invitation.title"
+                        ++ t "community.invitation.title"
                         ++ " "
                         ++ invite.community.title
 
@@ -88,30 +111,48 @@ view session model =
                     ""
 
         content =
-            div [ class "flex flex-col min-h-screen" ]
-                [ div [ class "flex-grow" ]
-                    [ case model.status of
-                        Loading ->
-                            div [] []
+            case model.pageStatus of
+                Loading ->
+                    text ""
 
-                        NotFound ->
-                            div [] []
+                NotFound ->
+                    text ""
 
-                        Failed e ->
-                            Page.fullPageGraphQLError (t shared.translations "") e
+                Failed e ->
+                    Page.fullPageGraphQLError (t "") e
 
-                        Loaded invite ->
-                            div []
-                                [ viewHeader
-                                , viewContent shared invite model.invitationId
-                                , viewModal shared model model.invitationId
-                                ]
+                Loaded invite ->
+                    let
+                        userCommunities =
+                            case session of
+                                LoggedIn { profile } ->
+                                    case profile of
+                                        LoggedIn.Loaded p ->
+                                            p.communities
 
-                        Error e ->
-                            Page.fullPageError (t shared.translations "") e
-                    ]
-                , viewFooter session
-                ]
+                                        _ ->
+                                            []
+
+                                Guest _ ->
+                                    []
+
+                        isMemberAlready : Bool
+                        isMemberAlready =
+                            -- True if current user is already a member of the inviter's community
+                            let
+                                isMember c =
+                                    symbolToString c.id == symbolToString invite.community.symbol
+                            in
+                            List.any isMember userCommunities
+                    in
+                    div []
+                        [ viewHeader
+                        , viewContent shared.translators invite model.invitationId isMemberAlready
+                        , viewModal shared.translators model.confirmationModalStatus model.invitationId
+                        ]
+
+                Error e ->
+                    Page.fullPageError (t "") e
     in
     { title = title
     , content = content
@@ -124,68 +165,87 @@ viewHeader =
         []
 
 
-viewContent : Shared -> Invite -> String -> Html Msg
-viewContent shared invite invitationId =
+viewContent : Translators -> Invite -> InvitationId -> Bool -> Html Msg
+viewContent { t, tr } { creator, community } invitationId isMemberAlready =
     let
         text_ s =
-            text (I18Next.t shared.translations s)
+            text (t s)
 
         inviter =
-            invite.creator.userName
-                |> Maybe.withDefault (Eos.nameToString invite.creator.account)
+            creator.userName
+                |> Maybe.withDefault (nameToString creator.account)
+
+        viewNewMemberConfirmation =
+            div []
+                [ div [ class "mt-6 px-4" ]
+                    [ span [ class "mr-1" ] [ text_ "community.invitation.subtitle" ]
+                    , text community.title
+                    , text "?"
+                    ]
+                , div [ class "flex flex-wrap justify-center w-full mt-6" ]
+                    [ button
+                        [ class "button button-secondary w-full md:w-48 uppercase mb-4 md:mr-8"
+                        , onClick OpenConfirmationModal
+                        ]
+                        [ text_ "community.invitation.no" ]
+                    , button
+                        [ class "button button-primary w-full md:w-48 uppercase"
+                        , onClick (AcceptInvitation invitationId)
+                        ]
+                        [ text_ "community.invitation.yes" ]
+                    ]
+                ]
+
+        viewExistingMemberNotice =
+            div [ class "mt-6" ]
+                [ p [] [ text (t "community.invitation.already_member") ]
+                , p [ class "max-w-lg md:mx-auto mt-3" ]
+                    [ text <|
+                        tr "community.invitation.choose_community_tip"
+                            [ ( "communityTitle", community.title ) ]
+                    ]
+                ]
     in
     div [ class "bg-white pb-20" ]
         [ div [ class "flex flex-wrap content-end" ]
             [ div [ class "flex items-center justify-center h-24 w-24 rounded-full mx-auto -mt-12 bg-white" ]
                 [ img
-                    [ src invite.community.logo
+                    [ src community.logo
                     , class "object-scale-down h-20 w-20"
                     ]
                     []
                 ]
             ]
-        , div [ class " px-4" ]
-            [ div [ class "flex mx-auto justify-center justify-center mt-6" ]
-                [ div [ class "inline-block text-center text-heading font" ]
-                    [ span [ class "mr-1 font-medium" ] [ text inviter ]
+        , div [ class "px-4 text-center text-heading" ]
+            [ div [ class "mt-6" ]
+                [ div [ class "font" ]
+                    [ span [ class "font-medium" ]
+                        [ text inviter
+                        ]
+                    , text " "
                     , text_ "community.invitation.title"
-                    , span [ class "ml-1 font-medium" ] [ text invite.community.title ]
+                    , text " "
+                    , span [ class "font-medium" ]
+                        [ text community.title ]
                     ]
                 ]
-            , div [ class "flex mx-auto justify-center mt-6 px-4" ]
-                [ div [ class "inline-block text-center text-heading" ]
-                    [ span [ class "mr-1" ] [ text_ "community.invitation.subtitle" ]
-                    , text invite.community.title
-                    , text "?"
-                    ]
-                ]
-            , div [ class "flex flex-wrap justify-center w-full mt-6" ]
-                [ button
-                    [ class "button button-sm button-secondary w-full md:w-48 uppercase mb-4 md:mr-8"
-                    , onClick OpenConfirmationModal
-                    ]
-                    [ text_ "community.invitation.no" ]
-                , button
-                    [ class "button button-sm button-primary w-full md:w-48 uppercase"
-                    , onClick (AcceptInvitation invitationId)
-                    ]
-                    [ text_ "community.invitation.yes" ]
-                ]
+            , if isMemberAlready then
+                viewExistingMemberNotice
+
+              else
+                viewNewMemberConfirmation
             ]
         ]
 
 
-viewModal : Shared -> Model -> String -> Html Msg
-viewModal shared model invitationId =
+viewModal : Translators -> ModalStatus -> InvitationId -> Html Msg
+viewModal { t } modalStatus invitationId =
     let
-        t s =
-            I18Next.t shared.translations s
-
         text_ s =
             text (t s)
 
         isModalVisible =
-            case model.confirmationModal of
+            case modalStatus of
                 Closed ->
                     False
 
@@ -215,20 +275,6 @@ viewModal shared model invitationId =
         |> Modal.toHtml
 
 
-viewFooter : Session -> Html msg
-viewFooter session =
-    let
-        shared =
-            case session of
-                LoggedIn loggedIn ->
-                    loggedIn.shared
-
-                Guest guest ->
-                    guest.shared
-    in
-    LoggedIn.viewFooter shared
-
-
 
 -- UPDATE
 
@@ -250,22 +296,22 @@ update : Session -> Msg -> Model -> UpdateResult
 update session msg model =
     case msg of
         CompletedLoad (Ok (Just invitation)) ->
-            UR.init { model | status = Loaded invitation }
+            UR.init { model | pageStatus = Loaded invitation }
 
         CompletedLoad (Ok Nothing) ->
-            UR.init { model | status = NotFound }
+            UR.init { model | pageStatus = NotFound }
 
         CompletedLoad (Err error) ->
-            { model | status = Failed error }
+            { model | pageStatus = Failed error }
                 |> UR.init
                 |> UR.logGraphqlError msg error
 
         OpenConfirmationModal ->
-            { model | confirmationModal = Open }
+            { model | confirmationModalStatus = Open }
                 |> UR.init
 
         CloseConfirmationModal ->
-            { model | confirmationModal = Closed }
+            { model | confirmationModalStatus = Closed }
                 |> UR.init
 
         RejectInvitation ->
@@ -313,8 +359,12 @@ update session msg model =
                     )
 
         CompletedSignIn _ (Err err) ->
-            { model | status = Error err }
+            { model | pageStatus = Error err }
                 |> UR.init
+
+
+
+-- INTEROP
 
 
 msgToString : Msg -> List String
