ea7c8f2ba8e32d21ac65a39aa5db2b032001662b
Rodrigo Leite <rodrigo@leite.dev>
2020-09-09 15:53:51 -0300

    Update graphql

--- src/elm/Cambiatus/Object/Country.elm
+++ src/elm/Cambiatus/Object/Country.elm
@@ -24,6 +24,6 @@ name =
     Object.selectionForField "String" "name" [] Decode.string
 
 
-states : SelectionSet decodesTo Cambiatus.Object.State -> SelectionSet (List (Maybe decodesTo)) Cambiatus.Object.Country
+states : SelectionSet decodesTo Cambiatus.Object.State -> SelectionSet (List decodesTo) Cambiatus.Object.Country
 states object_ =
-    Object.selectionForCompositeField "states" [] object_ (identity >> Decode.nullable >> Decode.list)
+    Object.selectionForCompositeField "states" [] object_ (identity >> Decode.list)
