1baf64f2b0d24c504d633add073f214815425dd0
Andrey Miskov <amiskov@gmail.com>
2021-02-11 01:08:16 +0300

    Get rid of ClaimWithPhoto module

--- src/elm/Route.elm
+++ src/elm/Route.elm
@@ -36,7 +36,6 @@ type Route
     | EditObjective Symbol Int
     | NewAction Symbol Int
     | EditAction Symbol Int Int
-      --| ClaimWithPhoto Symbol Int Int
     | Claim Int Int Int
     | Shop Shop.Filter
     | NewSale
@@ -98,8 +97,6 @@ parser url =
         , Url.map EditObjective (s "community" </> Eos.symbolUrlParser </> s "objectives" </> int </> s "edit")
         , Url.map NewAction (s "community" </> Eos.symbolUrlParser </> s "objectives" </> int </> s "action" </> s "new")
         , Url.map EditAction (s "community" </> Eos.symbolUrlParser </> s "objectives" </> int </> s "action" </> int </> s "edit")
-
-        --, Url.map ClaimWithPhoto (s "community" </> Eos.symbolUrlParser </> s "objectives" </> int </> s "action" </> int </> s "claim")
         , Url.map Claim (s "objectives" </> int </> s "action" </> int </> s "claim" </> int)
         , Url.map Shop
             (s "shop"
@@ -288,10 +285,6 @@ routeToString route =
                     , []
                     )
 
-                --ClaimWithPhoto symbol objectiveId actionId ->
-                --    ( [ "community", Eos.symbolToString symbol, "objectives", String.fromInt objectiveId, "action", String.fromInt actionId, "claim" ]
-                --    , []
-                --    )
                 Claim objectiveId actionId claimId ->
                     ( [ "objectives"
                       , String.fromInt objectiveId
