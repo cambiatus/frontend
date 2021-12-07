219080a29d08fe66e4912bc519306c45a7659a4f
Henrique Buss <henrique.buss@hotmail.com>
2021-12-07 10:43:37 -0300

    Fix according to elm-review

--- src/elm/View/Form/Select.elm
+++ src/elm/View/Form/Select.elm
@@ -4,7 +4,6 @@ module View.Form.Select exposing
     , toHtml
     , withAttrs
     , withContainerAttrs
-    , withLabelAttrs
     , withOption
     , withOptions
     )
@@ -51,7 +50,6 @@ init requiredOptions =
     , disabled = requiredOptions.disabled
     , extraAttrs = []
     , containerAttrs = []
-    , labelAttrs = []
     , problems = requiredOptions.problems
     }
 
@@ -64,11 +62,6 @@ withAttrs extraAttrs select =
 withContainerAttrs : List (Html.Attribute msg) -> Select a msg -> Select a msg
 withContainerAttrs attrs select =
     { select | containerAttrs = select.containerAttrs ++ attrs }
-
-
-withLabelAttrs : List (Html.Attribute msg) -> Select a msg -> Select a msg
-withLabelAttrs attrs select =
-    { select | labelAttrs = select.labelAttrs ++ attrs }
 
 
 {-| Adds a new option field to a dropdown
@@ -116,7 +109,7 @@ toHtml select =
             text ""
 
           else
-            View.Form.label select.labelAttrs select.id select.label
+            View.Form.label [] select.id select.label
         , Html.select
             (class "form-select w-full"
                 :: classList
@@ -156,7 +149,6 @@ type alias Select a msg =
     , disabled : Bool
     , extraAttrs : List (Html.Attribute msg)
     , containerAttrs : List (Html.Attribute msg)
-    , labelAttrs : List (Html.Attribute msg)
     , problems : Maybe (List String)
     }
 
