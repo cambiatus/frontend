75be5deaac419dc3ed2d56e22e98f530cceda5f8
Henrique Buss <henrique.buss@hotmail.com>
2022-03-28 11:49:25 -0300

    Add supporters card

--- src/elm/Page/Community/About.elm
+++ src/elm/Page/Community/About.elm
@@ -1,14 +1,17 @@
 module Page.Community.About exposing (Model, Msg, init, jsAddressToMsg, msgToString, update, view)
 
+import Avatar
 import Community
-import Html exposing (Html, a, button, div, h1, img, li, text, ul)
+import Html exposing (Html, a, button, div, h1, h2, img, li, p, span, text, ul)
 import Html.Attributes exposing (alt, class, href, media, src, style)
 import Html.Events exposing (onClick)
 import Icons
 import Json.Encode as Encode
+import List.Extra
 import Markdown
 import Profile.Contact as Contact
 import RemoteData
+import Route
 import Session.LoggedIn as LoggedIn
 import Translation
 import UpdateResult as UR
@@ -27,6 +30,7 @@ init : LoggedIn.Model -> UpdateResult
 init _ =
     UR.init {}
         |> UR.addExt (LoggedIn.RequestedCommunityField Community.UploadsField)
+        |> UR.addExt (LoggedIn.RequestedCommunityField Community.ContributionsField)
 
 
 
@@ -126,8 +130,9 @@ view loggedIn model =
 
                         _ ->
                             defaultCoverPhoto
-                    , div [ class "container mx-auto px-4 py-10" ]
+                    , div [ class "container mx-auto px-4 py-10 grid md:grid-cols-3 gap-6" ]
                         [ viewCommunityCard loggedIn.shared.translators community
+                        , viewSupportersCard loggedIn.shared.translators community
                         ]
                     ]
 
@@ -138,73 +143,147 @@ view loggedIn model =
 
 viewCommunityCard : Translation.Translators -> Community.Model -> Html Msg
 viewCommunityCard translators community =
-    div [ class "flex flex-col bg-white rounded relative px-4 pt-4 pb-6" ]
-        -- TW doesn't support translating on the x and y axies simultaneously,
-        -- so we need a parent element to center the icon horizontally
-        [ div [ class "absolute w-full top-0 left-0 flex justify-center -translate-y-1/2 pointer-events-none" ]
-            [ div
-                [ class "h-21 w-21 bg-white p-4 flex items-center justify-center rounded-full shadow-sm"
+    div []
+        -- We need some hidden text so the cards are aligned on desktop
+        [ p [ class "mb-4 opacity-0 pointer-events-none hidden md:block" ] [ text "hidden text" ]
+        , div [ class "flex flex-col bg-white rounded relative px-4 pt-4 pb-6" ]
+            -- TW doesn't support translating on the x and y axies simultaneously,
+            -- so we need a parent element to center the icon horizontally
+            [ div [ class "absolute w-full top-0 left-0 flex justify-center -translate-y-1/2 pointer-events-none" ]
+                [ div
+                    [ class "h-21 w-21 bg-white p-4 flex items-center justify-center rounded-full shadow-sm"
+                    ]
+                    [ img
+                        [ src community.logo
+                        , class "max-w-full max-h-full"
+
+                        -- TODO - Add alt explaining it's the community's logo
+                        , alt ""
+                        ]
+                        []
+                    ]
                 ]
-                [ img
-                    [ src community.logo
-                    , class "max-w-full max-h-full"
-
-                    -- TODO - Add alt explaining it's the community's logo
-                    , alt ""
-                    ]
-                    []
+            , button
+                [ class "bg-gray-100 p-2 rounded-full ml-auto focus-ring"
+                , onClick ClickedShareCommunity
                 ]
+                [ Icons.share "" ]
+            , h1 [ class "text-lg font-bold text-center" ] [ text community.name ]
+            , case community.website of
+                Nothing ->
+                    text ""
+
+                Just website ->
+                    let
+                        removeProtocol site =
+                            if String.startsWith "http://" site then
+                                -- Remove http://
+                                String.dropLeft 7 site
+
+                            else if String.startsWith "https://" website then
+                                -- Remove https://
+                                String.dropLeft 8 site
+
+                            else
+                                site
+
+                        removeTrailingSlash site =
+                            if String.endsWith "/" site then
+                                String.dropRight 1 site
+
+                            else
+                                site
+                    in
+                    a
+                        [ href website
+                        , class "mx-auto text-sm font-bold text-gray-900 hover:underline focus-ring"
+                        ]
+                        [ website
+                            |> removeProtocol
+                            |> removeTrailingSlash
+                            |> text
+                        ]
+            , Markdown.view [ class "mt-6" ] community.description
+            , ul [ class "flex flex-wrap mt-8 gap-x-6 gap-y-4 items-center justify-center" ]
+                (community.contacts
+                    |> List.map
+                        (\contact ->
+                            li [ class "w-10 h-10" ]
+                                [ Contact.circularIconWithGrayBg translators "" contact
+                                ]
+                        )
+                )
             ]
-        , button
-            [ class "bg-gray-100 p-2 rounded-full ml-auto focus-ring"
-            , onClick ClickedShareCommunity
-            ]
-            [ Icons.share "" ]
-        , h1 [ class "text-lg font-bold text-center" ] [ text community.name ]
-        , case community.website of
-            Nothing ->
-                text ""
+        ]
 
-            Just website ->
-                let
-                    removeProtocol site =
-                        if String.startsWith "http://" site then
-                            -- Remove http://
-                            String.dropLeft 7 site
 
-                        else if String.startsWith "https://" website then
-                            -- Remove https://
-                            String.dropLeft 8 site
+viewSupportersCard : Translation.Translators -> Community.Model -> Html Msg
+viewSupportersCard { t } community =
+    let
+        compareAvatars first second =
+            case ( Avatar.toMaybeString first, Avatar.toMaybeString second ) of
+                ( Just _, Just _ ) ->
+                    EQ
 
-                        else
-                            site
+                ( Just _, Nothing ) ->
+                    LT
 
-                    removeTrailingSlash site =
-                        if String.endsWith "/" site then
-                            String.dropRight 1 site
+                ( Nothing, Just _ ) ->
+                    GT
 
-                        else
-                            site
-                in
-                a
-                    [ href website
-                    , class "mx-auto text-sm font-bold text-gray-900 hover:underline focus-ring"
-                    ]
-                    [ website
-                        |> removeProtocol
-                        |> removeTrailingSlash
-                        |> text
-                    ]
-        , Markdown.view [ class "mt-6" ] community.description
-        , ul [ class "flex flex-wrap mt-8 gap-x-6 gap-y-4 items-center justify-center" ]
-            (community.contacts
+                ( Nothing, Nothing ) ->
+                    EQ
+
+        viewLoading =
+            List.range 0 4
                 |> List.map
-                    (\contact ->
-                        li [ class "w-10 h-10" ]
-                            [ Contact.circularIconWithGrayBg translators "" contact
+                    (\index ->
+                        div
+                            [ class "w-14 h-14 rounded-full -mr-2 border border-white bg-gray-300 animate-skeleton-loading last:mr-0"
+                            , style "animation-delay" (String.fromInt (index * 100) ++ "ms")
                             ]
+                            []
                     )
-            )
+    in
+    div []
+        [ h2 []
+            [ span [] [ text <| t "community.index.our_supporters" ]
+            , text " "
+            , span [ class "font-bold" ] [ text <| t "community.index.supporters" ]
+            ]
+        , div [ class "bg-white rounded px-4 pt-4 pb-6 mt-4" ]
+            [ p [] [ text <| t "community.index.you_can_support" ]
+            , a
+                [ class "button button-primary w-full mt-6"
+                , Route.href Route.CommunitySponsor
+                ]
+                [ text <| t "community.index.support_us" ]
+            , p [ class "mt-8 text-center" ] [ text <| t "community.index.see_supporters" ]
+            , div [ class "flex flex-wrap mt-4 justify-center" ]
+                (case community.contributions of
+                    RemoteData.Success contributions ->
+                        contributions
+                            |> List.map (.user >> .avatar)
+                            |> List.Extra.unique
+                            |> List.sortWith compareAvatars
+                            |> List.take 5
+                            |> List.map (\avatar -> Avatar.view avatar "w-14 h-14 object-cover rounded-full -mr-2 border border-white last:mr-0")
+
+                    RemoteData.Loading ->
+                        viewLoading
+
+                    RemoteData.NotAsked ->
+                        viewLoading
+
+                    RemoteData.Failure _ ->
+                        []
+                )
+            , a
+                [ class "button button-secondary w-full mt-4"
+                , Route.href Route.CommunitySupporters
+                ]
+                [ text <| t "community.index.see_all_supporters" ]
+            ]
         ]
 
 
