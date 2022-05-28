31f7992d83df7c0303674e1b82b5239a8309895c
Henrique Buss <henrique.buss@hotmail.com>
2022-05-27 21:23:54 -0300

    Adjust error case on profile editor image

--- src/elm/Page/Profile/Editor.elm
+++ src/elm/Page/Profile/Editor.elm
@@ -83,7 +83,7 @@ createForm ({ t } as translators) { hasKyc } =
             (Form.File2.init { id = "avatar-input" }
                 |> Form.File2.withContainerClass "grid place-items-center"
                 |> Form.File2.withImageClass "w-20 h-20 object-cover rounded-full mx-auto"
-                |> Form.File2.withEntryContainerClass "mx-auto"
+                |> Form.File2.withEntryContainerClass "mx-auto rounded-full"
                 |> Form.File2.withEditIconOverlay
                 |> Form.File2.withAddImagesView
                     [ span [ class "bg-orange-300 rounded-full p-4 w-20 h-20" ]
